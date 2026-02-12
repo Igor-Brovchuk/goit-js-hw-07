bb0ede3c9efdf8be4989a64047a5f3e763b56851
Igor-Brovchuk <i.brovchuk@gmail.com>
2026-02-12 17:02:49 +0200

    start

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categories = document.body.querySelector('#categories');
+const items = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${ items.length }`);
+
+
+
+
+
